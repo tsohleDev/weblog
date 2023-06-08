662f9b1a9fa581ff8a0ac674c1ecf2423f986330
tsohleDev <mokhemisitsohle@gmail.com>
2023-06-08 10:23:13 +0200

    Fix referencing

--- db/migrate/20230607202910_create_posts.rb
+++ db/migrate/20230607202910_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.text :text, null: false
       t.integer :comments_counter, default: 0
       t.integer :likes_counter, default: 0
-      t.references :user, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' }
 
       t.timestamps
     end
