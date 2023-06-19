7a30b42f078218410761c0335ff5a117886a01f7
tsohleDev <mokhemisitsohle@gmail.com>
2023-06-19 15:41:25 +0200

    Fix integration specs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,14 @@ class PostsController < ApplicationController
   end
 
   def show
-    @user = User.find_by_id(params[:user_id]) || not_found
-    @posts = @user.posts
-    @post = @posts.find_by_id(params[:id]) || not_found
-    @comments = @post.comments
+    # use includes to avoid n+1 queries
+    # find the user by id and include posts and comments
+    @user = User.includes(:posts, posts: [:comments])
+                .where(posts: { id: params[:id] })
+                .find_by_id(params[:user_id]) || not_found
+
+    # find the post by id
+    @post = @user.posts.find_by_id(params[:id]) || not_found
   end
 
   def new
